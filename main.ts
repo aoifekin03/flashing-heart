f90b043d85b92f6f1b3f6100915140199e5bb6e5
aoifekin03 <72395233+aoifekin03@users.noreply.github.com>
2020-10-05 14:30:03 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 basic.forever(function () {
+    basic.showArrow(ArrowNames.North)
+    basic.showIcon(IconNames.Heart)
     basic.showLeds(`
-        # . . . #
         . # . # .
-        . # # # .
+        # # # # #
+        # # # # #
         . # # # .
         . . # . .
         `)
